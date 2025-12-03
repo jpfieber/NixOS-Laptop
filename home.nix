694432544bcce99c6ddc19c579f1491f3d57c238
Joseph Fieber <jpfieber@gmail.com>
2025-12-03 15:58:26 -0600

    enabled home manager to overwrite files

--- home.nix
+++ home.nix
@@ -13,38 +13,50 @@
   
   # KDE screen lock timeout (in seconds)
   # Set to 0 to disable auto-lock, or change to desired seconds (e.g., 600 = 10 minutes)
-  home.file.".config/kscreenlockerrc".text = ''
-    [Daemon]
-    Autolock=true
-    LockOnResume=true
-    Timeout=0
-  '';
+  home.file.".config/kscreenlockerrc" = {
+    text = ''
+      [Daemon]
+      Autolock=true
+      LockOnResume=true
+      Timeout=0
+    '';
+    force = true;
+  };
   
   # Set desktop background to black
-  home.file.".config/plasma-org.kde.plasma.desktop-appletsrc".text = ''
-    [Containments][1][Wallpaper][org.kde.image][General]
-    Color=0,0,0
-    FillMode=1
-  '';
+  home.file.".config/plasma-org.kde.plasma.desktop-appletsrc" = {
+    text = ''
+      [Containments][1][Wallpaper][org.kde.image][General]
+      Color=0,0,0
+      FillMode=1
+    '';
+    force = true;
+  };
   
   # Enable Night Light with sunrise/sunset schedule
-  home.file.".config/kwinrc".text = ''
-    [NightColor]
-    Active=true
-    Mode=Times
-    NightTemperature=4500
-  '';
+  home.file.".config/kwinrc" = {
+    text = ''
+      [NightColor]
+      Active=true
+      Mode=Times
+      NightTemperature=4500
+    '';
+    force = true;
+  };
   
   # Set Breeze Dark theme and Oxygen icons
-  home.file.".config/kdeglobals".text = ''
-    [General]
-    ColorScheme=BreezeDark
-    
-    [Icons]
-    Theme=oxygen
-    
-    [KDE]
-    LookAndFeelPackage=org.kde.breezedark.desktop
-    widgetStyle=Breeze
-  '';
+  home.file.".config/kdeglobals" = {
+    text = ''
+      [General]
+      ColorScheme=BreezeDark
+      
+      [Icons]
+      Theme=oxygen
+      
+      [KDE]
+      LookAndFeelPackage=org.kde.breezedark.desktop
+      widgetStyle=Breeze
+    '';
+    force = true;
+  };
 }
