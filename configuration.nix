cc4ca04a8ade28915d3f38bd9539c4264a8fb23d
Joseph Fieber <jpfieber@gmail.com>
2025-12-03 19:08:45 -0600

    added plasma-manager

--- configuration.nix
+++ configuration.nix
@@ -6,6 +6,7 @@
 
 let
   home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-25.11.tar.gz";
+  plasma-manager = builtins.fetchTarball "https://github.com/nix-community/plasma-manager/archive/trunk.tar.gz";
 in
 {
   imports =
--- home.nix
+++ home.nix
@@ -8,39 +8,43 @@
     # Your user-specific packages would go here
   ];
   
-  # Import rclone mounts configuration
-  imports = [ ./rclone-mounts.nix ];
+  # Import rclone mounts and plasma-manager
+  imports = [ 
+    ./rclone-mounts.nix
+    <plasma-manager/modules>
+  ];
   
-  # KDE screen lock timeout (in seconds)
-  # Set to 0 to disable auto-lock, or change to desired seconds (e.g., 600 = 10 minutes)
-  home.file.".config/kscreenlockerrc" = {
-    text = ''
-      [Daemon]
-      Autolock=false
-      LockOnResume=false
-      Timeout=0
-      
-      [Greeter][Wallpaper][org.kde.image][General]
-      Image=
-    '';
-    force = true;
-  };
-  
-  # Also disable screen blanking in power management
-  home.file.".config/powermanagementprofilesrc" = {
-    text = ''
-      [AC][DPMSControl]
-      idleTime=0
-      lockBeforeTurnOff=0
-      
-      [AC][HandleButtonEvents]
-      lidAction=0
-      
-      [Battery][DPMSControl]
-      idleTime=0
-      lockBeforeTurnOff=0
-    '';
-    force = true;
+  # Declarative KDE Plasma configuration using plasma-manager
+  programs.plasma = {
+    enable = true;
+    
+    # Workspace settings
+    workspace = {
+      lookAndFeel = "org.kde.breezedark.desktop";
+      iconTheme = "oxygen";
+      colorScheme = "BreezeDark";
+      wallpaper = "${pkgs.kdePackages.plasma-workspace-wallpapers}/share/wallpapers/Kay/contents/images/1080x1920.png";
+    };
+    
+    # Screen locker
+    kscreenlocker = {
+      autoLock = false;
+      lockOnResume = false;
+      timeout = 0;
+    };
+    
+    # Power management
+    powerdevil = {
+      AC = {
+        autoSuspend = {
+          action = "nothing";
+          idleTimeout = 0;
+        };
+        turnOffDisplay = {
+          idleTimeout = 0;
+        };
+      };
+    };
   };
   
   # Disable KWallet password prompt
@@ -52,43 +56,6 @@
       Prompt on Open=false
       Close When Idle=false
       Idle Timeout=0
-    '';
-    force = true;
-  };
-  
-  # Set desktop background to black
-  home.file.".config/plasma-org.kde.plasma.desktop-appletsrc" = {
-    text = ''
-      [Containments][1][Wallpaper][org.kde.image][General]
-      Color=0,0,0
-      FillMode=1
-    '';
-    force = true;
-  };
-  
-  # Enable Night Light with sunrise/sunset schedule
-  home.file.".config/kwinrc" = {
-    text = ''
-      [NightColor]
-      Active=true
-      Mode=Times
-      NightTemperature=4500
-    '';
-    force = true;
-  };
-  
-  # Set Breeze Dark theme and Oxygen icons
-  home.file.".config/kdeglobals" = {
-    text = ''
-      [General]
-      ColorScheme=BreezeDark
-      
-      [Icons]
-      Theme=oxygen
-      
-      [KDE]
-      LookAndFeelPackage=org.kde.breezedark.desktop
-      widgetStyle=Breeze
     '';
     force = true;
   };
