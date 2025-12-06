ffea03332ef16111e11b7350296e35ecdd065827
Joseph Fieber <jpfieber@gmail.com>
2025-12-05 20:29:48 -0600

    nixos: apply vscode/home-manager changes

--- home-manager/vscode/default.nix
+++ home-manager/vscode/default.nix
@@ -15,93 +15,102 @@ in
       profiles.default = {
         # User settings
         userSettings = {
-        # Editor settings
-        "editor.formatOnSave" = true;
-        "editor.fontSize" = 14;
-        "editor.fontLigatures" = true;
-        "editor.linkedEditing" = true;
-        "editor.minimap.enabled" = true;
-        "editor.tabSize" = 2;
-        "editor.wordWrap" = "on";
-        
-        # Code actions
-        "editor.codeActionsOnSave" = {
-          "source.organizeImports" = "explicit";
-        };
-        
-        # Terminal
-        "terminal.integrated.fontFamily" = "monospace";
-        "terminal.integrated.fontSize" = 14;
-        
-        # Files
-        "files.autoSave" = "afterDelay";
-        "files.autoSaveDelay" = 1000;
-        "files.trimTrailingWhitespace" = true;
-        "files.insertFinalNewline" = true;
-        
-        # Git
-        "git.autofetch" = true;
-        "git.confirmSync" = false;
-        "git.enableSmartCommit" = true;
-        
-        # Workbench
-        "workbench.colorTheme" = "Default Dark Modern";
-        "workbench.iconTheme" = "vs-seti";
-        "workbench.startupEditor" = "none";
-        "workbench.tips.enabled" = false;
-        "workbench.welcome.enabled" = false;
-        
-        # Disable welcome/release notes
-        "update.showReleaseNotes" = false;
-        "workbench.welcomePage.walkthroughs.openOnInstall" = false;
-        
-        # Session management
-        "window.restoreWindows" = "all";
-        "files.restoreUndoStack" = true;
-        
-        # GitHub authentication
-        "github.gitAuthentication" = true;
-        "git.enableCommitSigning" = false;
-        
-        # GitHub Copilot settings
-        "github.copilot.enable" = {
-          "*" = true;
-        };
-        "github.copilot.editor.enableAutoCompletions" = true;
-        
-        # Extension recommendations
-        "extensions.ignoreRecommendations" = true;
-        "extensions.showRecommendationsOnlyOnDemand" = true;
-        
-        # Disable telemetry and updates (managed by Nix)
-        "telemetry.telemetryLevel" = "off";
-        "update.mode" = "none";
+          # Editor & formatting
+          "editor.formatOnSave" = true;
+          "editor.formatOnPaste" = true;
+          "editor.tabSize" = 2;
+          "editor.rulers" = [ 80 100 ];
+          "editor.codeActionsOnSave" = {
+            "source.organizeImports" = true;
+            "source.fixAll" = true;
+          };
+
+          # Visual / UX
+          "workbench.startupEditor" = "none";
+          "workbench.editor.enablePreview" = false;
+          "breadcrumbs.enabled" = true;
+          "explorer.compactFolders" = false;
+
+          # Files / performance
+          "files.trimTrailingWhitespace" = true;
+          "files.insertFinalNewline" = true;
+          "files.autoSave" = "afterDelay";
+          "files.autoSaveDelay" = 1000;
+
+          "files.watcherExclude" = {
+            "**/.git/**" = true;
+            "**/node_modules/**" = true;
+            "**/target/**" = true;
+            "**/.cache/**" = true;
+          };
+
+          "search.exclude" = {
+            "**/node_modules" = true;
+            "**/target" = true;
+          };
+
+          "files.exclude" = {
+            "**/.DS_Store" = true;
+            "**/.git" = true;
+          };
+
+          # Git / SCM
+          "git.autofetch" = true;
+          "git.enableSmartCommit" = true;
+          "git.confirmSync" = false;
+          "git.alwaysShowStagedChangesResourceGroup" = true;
+
+          # Terminal
+          "terminal.integrated.defaultProfile.linux" = "bash";
+          "terminal.integrated.fontFamily" = "monospace";
+          "terminal.integrated.fontSize" = 14;
+
+          # Linting / language specifics
+          "eslint.validate" = [ "javascript" "javascriptreact" "typescript" "typescriptreact" ];
+          "eslint.run" = "onSave";
+
+          "python.formatting.provider" = "black";
+          "python.formatting.blackArgs" = [ "--line-length" "88" ];
+
+          # Session / restore
+          "window.restoreWindows" = "all";
+          "files.restoreUndoStack" = true;
+
+          # Updates & telemetry (managed by Nix)
+          "update.mode" = "none";
+          "telemetry.telemetryLevel" = "off";
         };
         
         # Extensions
         extensions = with pkgs.vscode-extensions; [
-        # Nix language support
-        jnoortheen.nix-ide
-        
-        # Python
-        ms-python.python
-        ms-python.vscode-pylance
-        
-        # Node.js / JavaScript / TypeScript
-        dbaeumer.vscode-eslint
-        
-        # PowerShell
-        ms-vscode.powershell
-        
-        # Git
-        eamodio.gitlens
-        
-        # General development
-        editorconfig.editorconfig
-        
-        # Markdown
-        yzhang.markdown-all-in-one
-      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+          # Nix language support
+          jnoortheen.nix-ide
+
+          # Python
+          ms-python.python
+          ms-python.vscode-pylance
+
+          # Formatters / linters
+          esbenp.prettier-vscode
+          dbaeumer.vscode-eslint
+
+          # Git / SCM
+          eamodio.gitlens
+          mhutchie.git-graph
+
+          # Remote / container / docker
+          ms-azuretools.vscode-docker
+          ms-vscode-remote.remote-ssh
+
+          # Languages / LSPs
+          rust-lang.rust-analyzer
+
+          # Productivity / misc
+          streetsidesoftware.code-spell-checker
+          yzhang.markdown-all-in-one
+          editorconfig.editorconfig
+          ms-vscode.powershell
+        ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
         # Add additional extensions from marketplace here
         # Format: { name = "extension-name"; publisher = "publisher"; version = "x.x.x"; sha256 = "..."; }
       ];
