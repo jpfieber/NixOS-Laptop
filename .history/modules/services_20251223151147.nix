a500ae791fa63d8ce1a46af9e36505e1fdf8c6ad
Joseph Fieber <jpfieber@gmail.com>
2025-12-23 15:11:51 -0600

    feat: add comprehensive configuration for Brother printer, OpenSSH, and laptop lid behavior

---
+++ .history/modules/services_20251223151147.nix
@@ -0,0 +1,62 @@
+{ config, pkgs, ... }:
+
+{
+  # Enable CUPS to print documents.
+  services.printing = {
+    enable = true;
+    drivers = [ pkgs.brlaser ];  # Brother laser printer driver
+  };
+
+  # Pre-configure Brother printer
+  hardware.printers = {
+    ensurePrinters = [
+      {
+        name = "Brother_HL-L2340D";
+        location = "Network";
+        deviceUri = "ipp://192.168.86.19/ipp/print";
+        model = "drv:///brlaser.drv/brl2340d.ppd";
+        ppdOptions = {
+          PageSize = "Letter";
+        };
+      }
+    ];
+    ensureDefaultPrinter = "Brother_HL-L2340D";
+  };
+
+  # Avahi for network printer discovery
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
+
+  # Enable OpenSSH server (needed for sops-nix to use host keys)
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;  # More secure - use keys only
+      PermitRootLogin = "no";
+    };
+  };
+
+  # Configure laptop lid behavior - ignore lid close to keep system awake
+  services.logind = {
+    lidSwitch = "ignore";
+    lidSwitchDocked = "ignore";
+    lidSwitchExternalPower = "ignore";
+  };
+
+  # Sops secrets configuration - automatically deploys encrypted rclone.conf
+  # Only enable if the age key file exists
+  sops = {
+    defaultSopsFile = ../secrets/secrets.yaml;
+    age.keyFile = "/var/lib/sops-nix/key.txt";
+    validateSopsFiles = false;  # Don't fail if secrets can't be decrypted during install
+    secrets.rclone-conf = {
+      path = "/home/jpfieber/.config/rclone/rclone.conf";
+      owner = "jpfieber";
+      group = "users";
+      mode = "0600";
+    };
+  };
+}
