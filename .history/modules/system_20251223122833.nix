38f5b3a7b29097d1ff03f0fa632b32e4f93ee9b7
Joseph Fieber <jpfieber@gmail.com>
2025-12-23 12:28:42 -0600

    feat: add comprehensive NixOS installation and configuration scripts with enhanced safety checks and user prompts

---
+++ .history/modules/system_20251223122833.nix
@@ -0,0 +1,59 @@
+{ config, pkgs, ... }:
+
+{
+  # Enable flakes and increase download buffer
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    download-buffer-size = 134217728;  # 128MB
+  };
+
+  # Bootloader - UEFI configuration
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.useOSProber = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot";
+
+  # Swap configuration - helps prevent OOM kills during builds
+  swapDevices = [
+    {
+      device = "/var/lib/swapfile";
+      size = 4096; # 4GB swap file
+    }
+  ];
+
+  # Networking
+  networking.hostName = "nixos"; # Define your hostname.
+  networking.networkmanager.enable = true;
+
+  # Localization
+  time.timeZone = "America/Chicago";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Shell aliases for all users
+  environment.shellAliases = {
+    nrs = "cd /etc/nixos && git pull && sudo nixos-rebuild switch --flake .#nixos";
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.11"; # Did you read the comment?
+}
